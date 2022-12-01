fa8e9edc918a3a924d3dbbb2efef1ea2306705d2
Aleksandra Obukh <sem0609@mail.ru>
2022-12-01 17:55:44 +0300

    Add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "danil")
+{
+    Console.WriteLine("Ура, это же ДАНИЛ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
