eb2eac91c84f888a0bb8db76ef2d0014974ca5f8
Aleksandra Obukh <sem0609@mail.ru>
2022-12-03 16:28:04 +0300

    add example010

---
+++ Example010_IntroArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// найти индекс числа N
+
+int[] array = {7, 8, 13, 54, 13, 5, 93, 16};
+
+int n = array.Length;
+int find = 13;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
